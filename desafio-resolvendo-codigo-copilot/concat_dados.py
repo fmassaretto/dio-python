87d4df03f402944a90044db35549622cb14afe19
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-12-17 05:03:09 -0300

    Resolucao desafio resolvendo codigos com copilot

---
+++ desafio-resolvendo-codigo-copilot/concat_dados.py
@@ -0,0 +1,7 @@
+# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+user_input_1 = input("Digite um dado: ")
+user_input_2 = input("Digite outro dado: ")
+
+
+print(user_input_1 + user_input_2)
---
+++ desafio-resolvendo-codigo-copilot/ope_mat.py
@@ -0,0 +1,7 @@
+# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero_1 = int(input("Digite um numero inteiro: "))
+numero_2 = int(input("Digite outro numero inteiro: "))
+
+
+print(numero_1 * numero_2)
